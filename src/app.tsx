62f3a309b8c52311e580e8682afe8ba45e5888aa
Jean Nunes <jean.nunes@uzzipay.com>
2025-02-18 14:44:22 -0400

    Add widget structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,9 @@
+import { UploadWidget } from "./components/upload-widget";
+
 export default function App() {
-  return <>Hello World</>;
+  return (
+    <main className="h-dvh flex flex-col items-center justify-center p-10">
+      <UploadWidget />
+    </main>
+  );
 }
